49cd60938ede0b1e117faf93f07beef60f017206
FlaviouSs <flaviotubaroni@gmail.com>
2025-03-30 16:21:52 -0300

    fix: Wunzip is working as it should, still need to implement for multiples files

--- wunzip.c
+++ wunzip.c
@@ -6,14 +6,23 @@ int main (int argc, char *argv[]){
     char readed_char;
     int match;
 
-    while(fscanf(f, "%d%c", &match, &readed_char) != EOF){
-        for(int k = 0; k < match; k++){
+    if(argc == 1){
+        printf("wunzip: file1 [file2 ...]\n");
+        return 1;
+    }
+    
+    fread(&match, sizeof(int), 1, f);
+    fread(&readed_char, sizeof(char), 1, f);
+
+    while(!feof(f)){
+        for(int k = 1; k <= match; k++){
             printf("%c", readed_char);
         }
+
+        fread(&match, sizeof(int), 1, f);
+        fread(&readed_char, sizeof(char), 1, f);
     }
 
-    printf("\n");
     fclose(f);
-
     return 0;
 }
