69b6f9489c23a1044748848430e62f49072eb284
FlaviouSs <flaviotubaroni@gmail.com>
2025-03-30 16:36:38 -0300

    fix: Wunzp is working, all tests are OK

--- wunzip.c
+++ wunzip.c
@@ -1,28 +1,31 @@
 #include <stdio.h>
 
-int main (int argc, char *argv[]){
-
-    FILE *f = fopen(argv[1], "r");
-    char readed_char;
-    int match;
-
-    if(argc == 1){
+int main(int argc, char *argv[]) {
+    if (argc == 1) {
         printf("wunzip: file1 [file2 ...]\n");
         return 1;
     }
-    
-    fread(&match, sizeof(int), 1, f);
-    fread(&readed_char, sizeof(char), 1, f);
 
-    while(!feof(f)){
-        for(int k = 1; k <= match; k++){
-            printf("%c", readed_char);
-        }
+    for (int i = 1; i < argc; i++) {
+        FILE *f = fopen(argv[i], "r");
+
+        char readed_char;
+        int match;
 
         fread(&match, sizeof(int), 1, f);
         fread(&readed_char, sizeof(char), 1, f);
+
+        while (!feof(f)) {
+            for (int j = 1; j <= match; j++) {
+                printf("%c", readed_char);
+            }
+
+            fread(&match, sizeof(int), 1, f);
+            fread(&readed_char, sizeof(char), 1, f);
+        }
+
+        fclose(f);
     }
 
-    fclose(f);
     return 0;
 }
